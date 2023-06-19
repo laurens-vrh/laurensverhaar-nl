0962654e1522223b8624d23f8dacfded19678bac
Laurens <lauverhaar@gmail.com>
2023-06-19 07:16:54 +0200

    perf: :zap: Minify and purge built code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	outDir: "./dist",
+	integrations: [
+		purgecss(),
+		compress({
+			css: true,
+			html: true,
+			img: true,
+			js: true,
+			svg: true,
+			path: "./dist"
+		})
+	]
+});
